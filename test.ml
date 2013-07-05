2a3c6c844ae1c459bbb61b71c0663836939555a1
Carl Pulley <c.j.pulley@hud.ac.uk>
2013-07-05 21:23:10 +0100

    Added in some basic tests

---
+++ test.ml
@@ -0,0 +1,26 @@
+open OUnit
+open Unsigned
+
+let test_push_disasm4 () =
+  let rcode, size, mneumonic, operands, hex = Distorm.single_disasm Distorm.Decode32Bits "AAAA" in
+    assert_equal rcode Distorm.DECRES_MEMORYERR;
+    assert_equal size 1;
+    assert_equal mneumonic "INC";
+    assert_equal operands "ECX";
+    assert_equal hex "41"
+
+let test_push_disasm1 () =
+  let rcode, size, mneumonic, operands, hex = Distorm.single_disasm Distorm.Decode32Bits "A" in
+    assert_equal rcode Distorm.DECRES_SUCCESS;
+    assert_equal size 1;
+    assert_equal mneumonic "INC";
+    assert_equal operands "ECX";
+    assert_equal hex "41"
+
+let suite = "OUnit Distorm tests" >::: [
+  "test_push_disasm1" >:: test_push_disasm1;
+  "test_push_disasm4" >:: test_push_disasm4
+]
+
+let _ =
+  run_test_tt_main suite
